5a966cb13801daf02345d7c28302249ed66e4dfb
lmdube21 <103522848+lmdube21@users.noreply.github.com>
2023-02-07 13:53:50 -0600

    Update cfp_media_rights.R
    Start of Bowl Specific Analysis

--- cfp_media_rights.R
+++ cfp_media_rights.R
@@ -1,16 +1,57 @@
 ## ----include=FALSE------------------------------------------------------------------------------------------
 library(readr)
 library(mosaic)
+library(dplyr)
+library(ggthemes)
 
-path = "https://jaredsmurray.github.io/sta371g_f17/data/"
-salary = read_csv(paste0(path, 'salary_gender.csv'))
-# Turn the year hired variable into years of experience; the command
-# below makes a new column in the salary data frame called Exp
-salary$Exp = 96-salary$YrHired
+path = "/Users/lorenzo_dube/Documents/GitHub/CFP_Media_Rights/CFB_Bowl_Viewership.csv"
+viewership = read_csv(path)
+# create a separate dataframe with just the data from the CFP era
+cfp_only = viewership[viewership$Format == 'CFP' ,]
+#treat the game type as a categorical factor
+cfp_only$Viewers = as.numeric(cfp_only$Viewers)
+cfp_only$ROUND = factor(cfp_only$ROUND)
+cfp_no_nc = cfp_only[cfp_only$ROUND != "NC",]
 
-# This line ensures that R treats the Gender variable as categorical
-# when it comes time to fit regression models
-salary$Gender = factor(salary$Gender)
+cfp_no_nc <- cfp_no_nc %>%
+  mutate(ROUND = tolower(ROUND))
+
+semis = cfp_no_nc[cfp_no_nc$ROUND == 'semi',]
+nysix = cfp_no_nc[cfp_no_nc$ROUND == 'ny6',]
+  
+
+viewer_fit = lm(Viewers~ROUND+SEASON, data = cfp_no_nc)
+
+plotModel(viewer_fit, Viewers~SEASON)
+summary(viewer_fit)
+
+predicted_df <- data.frame(mpg_pred = predict(viewer_fit, cfp_no_nc), hp=df$hp)
+
+ggplot(viewer_fit, aes(SEASON, Viewers)) +
+  geom_point() + 
+  geom_line(aes(SEASON,viewer_fit)) +
+  scale_color_fivethirtyeight("ROUND") +
+  theme_fivethirtyeight()
+
+ggplot(cfp_no_nc,aes(y = Viewers, x = SEASON, shape = ROUND, colour = ROUND)) +
+  geom_point() + geom_smooth(method = "lm", fill = NA) + scale_color_fivethirtyeight("ROUND") + theme_fivethirtyeight()
+
+ggplot() + 
+  geom_smooth(method = 'lm', data=nysix, aes(x=SEASON, y=Viewers)) + 
+  
+  geom_smooth(method = 'lm', data=semis, aes(x=SEASON, y=Viewers)) +
+  scale_color_fivethirtyeight("ROUND") +
+  theme_fivethirtyeight()
+  
+  
+
+
+  
+  
+
+
+
+
 
 # The model from last time
 salaryfit_exp = lm(Salary~Gender+Exp, data=salary)
@@ -18,6 +59,8 @@ salaryfit_exp = lm(Salary~Gender+Exp, data=salary)
 plotModel(salaryfit_exp, Salary~Exp)
 
 salaryfit_int = lm(Salary~Gender*Exp, data=salary)
+
+
 
 summary(salaryfit_int)
 
